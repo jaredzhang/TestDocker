e5896d40caf7cc0cdd540df84882473fdbab2722
jaredzhang <jaredzhang09@gmail.com>
2016-09-27 12:05:32 +0800

    Update script

--- flow.groovy
+++ flow.groovy
@@ -4,33 +4,33 @@ node() {
   stage 'Checkout'
   checkout scm
 
+  prefix = 'docker run -i --rm --volume=${PWD}:/opt/workspace --workdir=/opt/workspace jaredzhang09/android-docker:latest'
   stage 'Create Env'
-  def buildEnv = docker.image('jaredzhang09/android-coker:latest').withRun('-it --volume=$(pwd):/opt/workspace --workdir=/opt/workspace ') {
-      
+  //docker.image('jaredzhang09/android-docker:latest').inside('-it --volume=$(pwd):/opt/workspace --workdir=/opt/workspace ') { c ->  
     stage 'Build'
-    sh 'gradlew clean assembleDebug'
+    sh "${prefix} ls"
+    sh "${prefix} pwd"
+    sh "${prefix} gradle assembleDebug"
     archive 'app/build/outputs/**/app-debug.apk'
 
     stage 'Quality'
-    sh './gradlew lint'
+    sh "${prefix} gradle lint"
     stash includes: '*/build/outputs/lint-results*.xml', name: 'lint-reports'
 
     stage 'Test (unit)'
     try {
-      sh './gradlew test'
+      sh "${prefix} gradle test"
     } catch (err) {
         currentBuild.result = 'UNSTABLE'
     }
     stash includes: '**/test-results/**/*.xml', name: 'junit-reports'
 
     stage 'Test (device)'
-    sh '''gradlew :app:assembleDebug
-          gradlew :app:assembleDebugAndroidTest
-       '''
+    sh "${prefix} gradle :app:assembleDebug :app:assembleDebugAndroidTest"
     // Archive for downstream AWS job
     archive 'app/build/outputs/**/*androidTest*.apk'
   }
-}
+//}
 
 node() {
   build "${env.JOB_NAME} (AWS)"
