b0136e3c23cdf65d611e6a6b06f849eb726cc378
jaredzhang <jaredzhang09@gmail.com>
2016-09-28 21:43:24 +0800

    --amend

--- flow.groovy
+++ flow.groovy
@@ -1,39 +1,48 @@
 node() {
   deleteDir()
 
-  stage 'Checkout'
-  checkout scm
+  stage ('Checkout') {
+    checkout scm
+  }
 
-  //prefix = 'docker run -i --rm --volume=${PWD}:/opt/workspace --workdir=/opt/workspace jaredzhang09/android-docker:latest'
-  prefix=''
-  stage 'Create Env'
-  docker.image('jaredzhang09/android-docker:latest').inside('-it --volume=$(pwd):/opt/workspace --workdir=/opt/workspace ') { c ->  
-    stage 'Build'
-    sh "${prefix} gradle clean assembleDebug"
-    archive 'app/build/outputs/**/app-debug.apk'
+  //prefix = 'docker run -i --rm jaredzhang09/android-docker:latest'
+  stage ('Create Env') {
+    docker.image('jaredzhang09/android-docker-minimum:latest').inside {  
+      
+      stage ('Build') {
+        sh "pwd"
+        sh "ls"
+        sh "id"
+        sh "./gradlew clean assembleDebug"
+        archive 'app/build/outputs/**/app-debug.apk'
+      }
 
-    stage 'Quality'
-    sh "${prefix} gradle lint"
-    stash includes: '*/build/outputs/lint-results*.xml', name: 'lint-reports'
+      stage ('Quality') {
+        sh "./gradlew lint"
+        stash includes: '*/build/outputs/lint-results*.xml', name: 'lint-reports'
+      }
+   
+      stage ('Test (unit)') {
+        try {
+          sh "./gradlew test"
+        } catch (err) {
+            currentBuild.result = 'UNSTABLE'
+        }
+        stash includes: '**/test-results/**/*.xml', name: 'junit-reports'
+      }
 
-    stage 'Test (unit)'
-    try {
-      sh "${prefix} gradle test"
-    } catch (err) {
-        currentBuild.result = 'UNSTABLE'
+      stage ('Test (device)') {
+        sh "./gradlew :app:assembleDebug :app:assembleDebugAndroidTest"
+        // Archive for downstream AWS job
+        archive 'app/build/outputs/**/*androidTest*.apk'
+      }
     }
-    stash includes: '**/test-results/**/*.xml', name: 'junit-reports'
-
-    stage 'Test (device)'
-    sh "${prefix} gradle :app:assembleDebug :app:assembleDebugAndroidTest"
-    // Archive for downstream AWS job
-    archive 'app/build/outputs/**/*androidTest*.apk'
   }
 }
 
-node() {
-  build "${env.JOB_NAME} (AWS)"
-}
+//node() {
+//  build "${env.JOB_NAME} (AWS)"
+//}
 
 stage 'Report'
 node() {
