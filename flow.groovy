0f5dda911301faa7cc0e4d4ea435a8e113e0a97d
jaredzhang <jaredzhang09@gmail.com>
2016-10-07 12:06:38 +0800

    Add a new script

--- flow.groovy
+++ flow.groovy
@@ -13,26 +13,6 @@ node() {
         sh "./gradlew clean assembleDebug"
         archive 'app/build/outputs/**/app-debug.apk'
       }
-
-      stage ('Quality') {
-        sh "./gradlew lint"
-        stash includes: '*/build/outputs/lint-results*.xml', name: 'lint-reports'
-      }
-   
-      stage ('Test (unit)') {
-        try {
-          sh "./gradlew test"
-        } catch (err) {
-            currentBuild.result = 'UNSTABLE'
-        }
-        stash includes: '**/test-results/**/*.xml', name: 'junit-reports'
-      }
-
-      stage ('Test (device)') {
-        sh "./gradlew :app:assembleDebug :app:assembleDebugAndroidTest"
-        // Archive for downstream AWS job
-        archive 'app/build/outputs/**/*androidTest*.apk'
-      }
     }
   }
 }
@@ -40,14 +20,3 @@ node() {
 //node() {
 //  build "${env.JOB_NAME} (AWS)"
 //}
-
-stage 'Report'
-node() {
-  deleteDir()
-
-  unstash 'junit-reports'
-  step([$class: 'JUnitResultArchiver', testResults: '**/test-results/**/*.xml'])
-
-  unstash 'lint-reports'
-  step([$class: 'LintPublisher', canComputeNew: false, canRunOnFailed: true, defaultEncoding: '', healthy: '', pattern: '*/build/outputs/lint-results*.xml', unHealthy: ''])
-}
